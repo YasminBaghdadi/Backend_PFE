384e686cf91d4b59bc9a80899d4b890b1bafc0b3
baghdadi1 <160177756+baghdadi1@users.noreply.github.com>
2025-03-11 11:17:21 +0100

    Initial commit

--- src/main/java/com/example/pfe/Controller/UserController.java
+++ src/main/java/com/example/pfe/Controller/UserController.java
@@ -1,12 +1,13 @@
 package com.example.pfe.Controller;
 
+
 import com.example.pfe.Entity.User;
 import com.example.pfe.Services.UserInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/projet")
@@ -30,14 +31,12 @@ public class UserController {
     {
         return userInterface.addUserWTCP(user);
     }
-
     // VERIFICATION 1 username existe //
     @PostMapping("/username")
     public String addUserWTUN(@RequestBody User user)
     {
         return userInterface.addUserWTUN(user);
     }
-
     // modifier //
     @PutMapping("/updateuser/{ids}")
     public User updateuser(@PathVariable("ids")Long idUser,@RequestBody User user)
@@ -48,8 +47,6 @@ public class UserController {
     public List<User> getAllUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
         return userInterface.getAllusers(page, size);
     }
-
-
     @GetMapping("getUserById/{idUser}")
     public User getUserById(@PathVariable Long idUser)
     {
--- src/main/java/com/example/pfe/Repository/UserRepository.java
+++ src/main/java/com/example/pfe/Repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.pfe.Repository;
 
 import com.example.pfe.Entity.Rolename;
 import com.example.pfe.Entity.User;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
